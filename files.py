c94b1b7e4b2bea0ca6963152c39a39cba40fe354
mopheshi <ndachimya@gmail.com>
2023-01-27 15:47:42 +0100

    ...exercises!

--- files.py
+++ files.py
@@ -1,1 +1,7 @@
-open('mop.txt', 'w')
+file = open('mop.txt', 'w')
+l1 = file.write("This is line one...")
+l2 = file.write("\nThis is line two...")
+x = 23
+l3 = file.write(f"\nThis is line three with an integer {'%d' % x}.")
+# l3 = file.write(f"\nThis is line three with an integer {str(x)}.")
+file.close()
