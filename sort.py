f7a19d7aadd0419a682564ab7bac95f0259b5fb1
mopheshi <ndachimya@gmail.com>
2023-02-27 00:30:55 +0100

    ...exercises!

--- sort.py
+++ sort.py
@@ -1,6 +1,14 @@
-li = [12, 45, 234, 75, 2, 43]
 iterator = 0
-temporaryInteger = 0;
+temporaryInteger = 0
+
+# You can use this to get user inputs
+# print("Type in 3 integer values, abeg: ")
+# for i in range(0, 3):
+#     integer = int(input())
+#     li.append(integer)
+
+# Getting user inputs
+li = [int(integer) for integer in input("Enter the 3 integers separated with a single space ' ', biko...: ").split()]
 
 print("Unsorted list =", li)
 
@@ -11,4 +19,4 @@ for i in range(iterator, len(li)):
             li[i] = li[j]
             li[j] = temporaryInteger
 
-print("Sorted list =", li)
+print("Sorted list =", li, "\nSmallest =", li[0], "\nMedium =", li[1], "\nHighest =", li[2])
