f5e200f30a1190f9ded87ea0714ef9a8f08746b5
mopheshi <ndachimya@gmail.com>
2023-05-09 21:18:52 +0100

    ...Binary Search with list...

---
+++ binarySearchArrayList.py
@@ -0,0 +1,19 @@
+import bisect
+
+# with open('Files/english.txt', 'r') as f:
+#     lines = f.readlines()
+#     lines = [line.strip() for line in lines]
+#     lines.sort()
+#
+# print(lines)
+
+with open('Files/english.txt', 'r') as file:
+    l = [line.strip() for line in file]
+
+l = sorted(l)
+print(l)
+
+item = input("What do you want to search? ")
+index = bisect.bisect_left(l, item)
+
+print(f"{item} exists at index {index}")
