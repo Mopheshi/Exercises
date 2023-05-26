eaa989855dc97cdf48a1bdaeaba3e711ca40d084
mopheshi <ndachimya@gmail.com>
2023-05-26 13:30:29 +0100

    ...topological sort...

--- TopologicalSort.py
+++ TopologicalSort.py
@@ -1,4 +1,7 @@
-from collections import defaultdict
+"""
+A topological sort or topological ordering of a directed graph is a linear ordering of its vertices such that for 
+every directed edge uv from vertex u to vertex v, u comes before v in the ordering.
+"""
 
 
 def topologicalSort(graph):
@@ -20,7 +23,7 @@ def topologicalSort(graph):
 
 
 # Example usage:
-graph = {
+graph_sample = {
     'A': ['B', 'C'],
     'B': ['D'],
     'C': ['D', 'E'],
@@ -29,7 +32,7 @@ graph = {
     'F': []
 }
 
-result = topologicalSort(graph)
+result = topologicalSort(graph_sample)
 print(result)
 
 """
