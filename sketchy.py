eb493fb1f1c3267b8f85416992d4e4f332f6bbbd
mopheshi <ndachimya@gmail.com>
2023-06-21 15:09:31 +0100

    ...sketchpy implementation...

---
+++ sketchy.py
@@ -0,0 +1,27 @@
+from sketchpy import library as lib
+
+
+def drawRobertDowneyJr():
+    obj = lib.rdj()
+    obj.draw()
+
+
+def drawIronMan():
+    obj = lib.ironman_ascii()
+    obj.draw()
+
+
+def drawBTS():
+    obj = lib.bts()
+    obj.draw()
+
+
+def drawGojo():
+    obj = lib.gojo()
+    obj.draw()
+
+
+# drawRobertDowneyJr()
+drawIronMan()
+# drawBTS()
+# drawGojo()
