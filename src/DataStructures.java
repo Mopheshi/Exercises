eb775a0ffd1b087908be14518b49154516fd580a
mopheshi <ndachimya@gmail.com>
2024-04-17 14:26:10 +0100

    DataStructures...

---
+++ src/DataStructures.java
@@ -0,0 +1,43 @@
+public class DataStructures {
+//Linked List:
+//a. Implement a singly linked list in Java with methods to:
+//Insert a node at the beginning/end of the list.
+//Delete a node with a given value.
+//Search for a node with a given value.
+//Traverse the list and print its elements.
+//b. Implement a doubly linked list in Java with similar functionalities.
+//c. Provide the time complexity analysis for each operation in both singly and doubly linked lists.
+
+//Stack:
+//a. Implement a stack data structure in Java using an array.
+//b. Implement push and pop operations for the stack.
+//c. Write a method to check if a given string containing parentheses (, ), {, }, [, or ] is balanced using a stack.
+//d. Discuss the advantages and disadvantages of using arrays vs. linked lists to implement a stack.
+
+//Queue:
+//a. Implement a queue data structure in Java using an array.
+//b. Implement enqueue and dequeue operations for the queue.
+//c. Implement a circular queue to efficiently use the underlying array space.
+//d. Discuss the trade-offs between array-based and linked list-based implementations of queues.
+
+
+//Binary Search Tree (BST):
+//a. Implement a binary search tree in Java with methods to:
+//Insert a node.
+//Delete a node with a given value.
+//Search for a node with a given value.
+//Traverse the tree in-order, pre-order, and post-order.
+//b. Write a method to find the minimum and maximum values in a BST.
+//c. Discuss the time complexity of each operation in a BST and the conditions under which the tree degenerates into a linked list.
+
+    //Hash Table:
+//a. Implement a hash table data structure in Java with methods to:
+//Insert a key-value pair.
+//Retrieve the value associated with a given key.
+//Remove a key-value pair.
+//b. Implement collision resolution techniques such as chaining or open addressing.
+//c. Discuss the factors affecting the choice of hash function and the collision resolution strategy.
+    public static void main(String[] args) {
+
+    }
+}
