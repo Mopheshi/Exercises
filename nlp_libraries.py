1fa456b4e0b19169c47d96fa5658f934c5f34d03
mopheshi <ndachimya@gmail.com>
2024-05-17 14:31:52 +0100

    Beginning text summarization...

--- nlp_libraries.py
+++ nlp_libraries.py
@@ -1,10 +1,15 @@
 import nltk
+from nltk.corpus import stopwords
 import spacy
-from nltk import word_tokenize, pos_tag
+from nltk import word_tokenize, pos_tag, sent_tokenize
 from nltk.corpus import twitter_samples
 from spacytextblob.spacytextblob import SpacyTextBlob
+from collections import defaultdict
+import heapq
 
-from vars import message, positive_words, negative_words, reviews, categories
+# from gensim.summarization import summarize
+
+# from vars import message, positive_words, negative_words, reviews, categories
 
 # Download the models if not already installed
 # nltk.download("popular")
@@ -148,6 +153,84 @@ def text_classification_spacy(text):
     return predicted_category  # Return the predicted category
 
 
+def summarize_nltk(text, num_sentences):
+    """
+    Function to summarize a given text document using NLTK.
+    :param num_sentences: Number of sentences to include in the summary.
+    :param text: Input text to be summarized.
+    :return: Summary of the text document.
+    """
+    # Tokenize the text into sentences
+    sentences = sent_tokenize(text)
+
+    # Tokenize the text into words and remove stopwords
+    stop_words = set(stopwords.words("english"))
+    words = word_tokenize(text)
+    words = [word.lower() for word in words if word.isalnum()]
+    words = [word for word in words if word not in stop_words]
+
+    # Calculate word frequencies
+    word_freq = defaultdict(int)
+    for word in words:
+        word_freq[word] += 1
+
+    # Calculate the sentence scores
+    sentence_scores = defaultdict(int)
+    for sentence in sentences:
+        sentence_words = word_tokenize(sentence.lower())
+        for word in sentence_words:
+            if word in word_freq:
+                sentence_scores[sentence] += word_freq[word]
+
+    # Get the highest scored sentences
+    summary_sentences = heapq.nlargest(num_sentences, sentence_scores, key=sentence_scores.get)
+
+    # Join the sentences into a summary
+    summary = ' '.join(summary_sentences)
+    return summary
+
+
+def summarize_spacy(text, num_sentences):
+    """
+    Function to summarize a given text document using spaCy.
+    :param text: Input text to be summarized.
+    :return: Summary of the text document.
+    """
+    doc = nlp(text)
+    sentences = list(doc.sents)
+
+    # Calculate word frequencies
+    word_freq = defaultdict(int)
+    for word in doc:
+        if not word.is_stop and not word.is_punct:
+            word_freq[word.text.lower()] += 1
+
+        # Calculate the sentence scores
+    sentence_scores = defaultdict(int)
+    for sent in sentences:
+        for word in sent:
+            if word.text.lower() in word_freq:
+                sentence_scores[sent] += word_freq[word.text.lower()]
+
+    # Get the highest scored sentences
+    summary_sentences = heapq.nlargest(num_sentences, sentence_scores, key=sentence_scores.get)
+
+    # Join the sentences into a summary
+    summary = ' '.join([sent.text for sent in summary_sentences])
+    return summary
+
+
+def summarize_gensim(text, ratio):
+    """
+    Function to summarize a given text document using Gensim.
+    :param ratio: The ratio of the number of sentences in the summary to the number of sentences in the original text.
+    :param text: Input text to be summarized.
+    :return: Summary of the text document.
+    """
+    # summary = summarize(text, ratio=ratio)
+    # return summary
+
+
 # print(pos_tagging_nltk(message))
 # print(pos_tagging_spacy(message))
 # print(ner_nltk(message))
@@ -162,26 +245,24 @@ def text_classification_spacy(text):
 # print(sentiment_analysis_spacy(reviews))
 
 # print(text_classification_nltk(message))
-print(text_classification_spacy(message))
+# print(text_classification_spacy(message))
 
 
-# Text Summarization:
-# a. Write a script using NLTK to summarize a given text document by extracting the most important sentences.
-# b. Experiment with spaCy to build a text summarization model that generates concise summaries of news articles or
-# research papers.
-def summarize_nltk(text):
-    """
-    Function to summarize a given text document using NLTK.
-    :param text: Input text to be summarized.
-    :return: Summary of the text document.
-    """
-    pass
+file = 'files/sample_texts.txt'
+with open(file, 'r') as f:
+    text = f.read()
 
+    print(text, '\n')
 
-def summarize_spacy(text):
-    """
-    Function to summarize a given text document using spaCy.
-    :param text: Input text to be summarized.
-    :return: Summary of the text document.
-    """
-    pass
+    num_sentences = int(
+        input("How many sentences would like the above text to be summarized to? (e.g., 1, 2, 3,...): ")
+    )
+
+    print('NLTK Summary:')
+    print(summarize_nltk(text, num_sentences), '\n')
+
+    print('SpaCy Summary:')
+    print(summarize_spacy(text, num_sentences), '\n')
+
+    # print("Gensim Summary:")
+    # print(summarize_gensim(text, 0.2), '\n')
