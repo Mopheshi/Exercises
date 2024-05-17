0703e581c413e75be06b521f4efe468dbcf1de62
mopheshi <ndachimya@gmail.com>
2024-05-17 14:32:36 +0100

    Beginning text summarization...

--- nlp_libraries.py
+++ nlp_libraries.py
@@ -7,9 +7,9 @@ from spacytextblob.spacytextblob import SpacyTextBlob
 from collections import defaultdict
 import heapq
 
-# from gensim.summarization import summarize
+from gensim.summarization import summarize
 
-# from vars import message, positive_words, negative_words, reviews, categories
+from vars import message, positive_words, negative_words, reviews, categories
 
 # Download the models if not already installed
 # nltk.download("popular")
